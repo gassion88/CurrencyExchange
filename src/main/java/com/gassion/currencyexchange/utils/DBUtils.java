c54c6ea6e08bc35f94f43f766ef1f1a5a8e9071d
Soslan Gassiev <soslangassity04@gmail.com>
2023-05-04 00:05:20 +0300

    feat : add DB query constants

---
+++ src/main/java/com/gassion/currencyexchange/utils/DBUtils.java
@@ -0,0 +1,14 @@
+package com.gassion.currencyexchange.utils;
+
+import com.gassion.currencyexchange.DB.SQLiteConnection;
+
+import java.sql.Connection;
+
+public class DBUtils {
+    public static Connection connection = SQLiteConnection.getConnection();
+    public static final String SELECT = "SELECT * FROM %s WHERE %s = ?;";
+    public static final String SELECT_ALL = "SELECT * FROM %s;";
+    public static final String INSERT = "INSERT INTO %s (%s, %s, %s) VALUES (?, ?, ?);";
+    public static final String UPDATE = "UPDATE %s SET %s = ?, %s = ?, %s = ? WHERE ID = ?;";
+    public static final String DELETE = "DELETE FROM %s WHERE ID = ?;";
+}
