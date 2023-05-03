bb89ed2a9ddfb8468a609b4138013d647b2179ff
Soslan Gassiev <soslangassity04@gmail.com>
2023-05-03 18:27:23 +0300

    fix : add parametr id to method

--- src/main/java/com/gassion/currencyexchange/DAO/DAO.java
+++ src/main/java/com/gassion/currencyexchange/DAO/DAO.java
@@ -3,7 +3,7 @@ package com.gassion.currencyexchange.DAO;
 import java.util.List;
 
 public abstract class DAO <T> {
-    public abstract T get();
+    public abstract T get(int id);
     public abstract List<T> getAll();
     public abstract void set(T t);
     public abstract void add(T t);
