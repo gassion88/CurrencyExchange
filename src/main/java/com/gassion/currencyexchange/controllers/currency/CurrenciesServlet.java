01707451fce784917a57433a0c165a46ee3ec41d
Soslan Gassiev <soslangassity04@gmail.com>
2023-05-05 15:56:45 +0300

    feat : add GET/currencies

--- src/main/java/com/gassion/currencyexchange/controllers/currency/CurrenciesServlet.java
+++ src/main/java/com/gassion/currencyexchange/controllers/currency/CurrenciesServlet.java
@@ -1,5 +1,7 @@
 package com.gassion.currencyexchange.controllers.currency;
 
+import com.gassion.currencyexchange.logic.CurrencyLogic;
+import com.google.gson.Gson;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -10,11 +12,10 @@ import java.io.IOException;
 public class CurrenciesServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        CurrencyLogic.getAllCurrencies(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
     }
 }
