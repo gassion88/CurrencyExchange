6f892c5f3baa1b0c5f2ebeecd50cee10cdfd2977
Soslan Gassiev <soslangassity04@gmail.com>
2023-05-05 17:48:19 +0300

    fix : fix response fiel fullName to name

--- src/main/java/com/gassion/currencyexchange/logic/CurrencyLogic.java
+++ src/main/java/com/gassion/currencyexchange/logic/CurrencyLogic.java
@@ -2,21 +2,25 @@ package com.gassion.currencyexchange.logic;
 
 import com.gassion.currencyexchange.DAO.CurrencyDAO;
 import com.gassion.currencyexchange.entities.Currency;
+import com.gassion.currencyexchange.entities.jsonResponse.CurrencyJson;
+import com.gassion.currencyexchange.utils.ValidateUtils;
 import com.google.gson.Gson;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 import java.util.List;
 
 public class CurrencyLogic {
     private static final Gson GSON = new Gson();
+    private static final ValidateUtils VALIDATE_UTILS = new ValidateUtils();
 
     public static void getAllCurrencies(HttpServletRequest request, HttpServletResponse response) throws IOException {
         List<Currency> currencies = new CurrencyDAO().getAll();
-        String currenciesList = GSON.toJson(currencies);
-        System.out.println("133");
+        List<CurrencyJson> currenciesJson = ValidateUtils.getJsonFormat(currencies);
+        String currenciesList = GSON.toJson(currenciesJson);
 
         PrintWriter out = response.getWriter();
         response.setContentType("application/json");
@@ -26,7 +30,15 @@ public class CurrencyLogic {
         out.flush();
     }
 
-    public static void addCurrency(HttpServletRequest request, HttpServletResponse response) {
+    public static void addCurrency(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        VALIDATE_UTILS.addCurrencyRequestValidate(request, response);
 
+        Currency currency  = Currency.currencyFactory(request.getParameterMap());
+
+        try {
+            new CurrencyDAO().add(currency);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
