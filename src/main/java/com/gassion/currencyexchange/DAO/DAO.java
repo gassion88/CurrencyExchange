59b89c5b38578396e8a8bc94d63fcaed505a8962
Soslan Gassiev <soslangassity04@gmail.com>
2023-05-06 15:01:01 +0300

    fix :
    Added SQLException to handle above errors in code

--- src/main/java/com/gassion/currencyexchange/DAO/DAO.java
+++ src/main/java/com/gassion/currencyexchange/DAO/DAO.java
@@ -1,11 +1,12 @@
 package com.gassion.currencyexchange.DAO;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public abstract class DAO <T> {
-    public abstract T get(int id);
+    public abstract T get(String code);
     public abstract List<T> getAll();
     public abstract void set(T t);
-    public abstract void add(T t);
+    public abstract void add(T t) throws SQLException;
     public abstract void delete(int id);
 }
