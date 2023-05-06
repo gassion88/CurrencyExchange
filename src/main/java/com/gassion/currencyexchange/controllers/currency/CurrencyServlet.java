04cdf918e3eaef0633583fdd3b9399d411c35bb6
Soslan Gassiev <soslangassity04@gmail.com>
2023-05-06 17:59:19 +0300

    feat :

--- src/main/java/com/gassion/currencyexchange/controllers/currency/CurrencyServlet.java
+++ src/main/java/com/gassion/currencyexchange/controllers/currency/CurrencyServlet.java
@@ -1,16 +1,20 @@
 package com.gassion.currencyexchange.controllers.currency;
 
+import com.gassion.currencyexchange.DAO.CurrencyDAO;
+import com.gassion.currencyexchange.entities.Currency;
+import com.gassion.currencyexchange.logic.CurrencyLogic;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet(name = "CurrencyServlet", value = "/currency")
+@WebServlet(name = "CurrencyServlet", value = "/currency/*")
 public class CurrencyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        CurrencyLogic.getCurrencyByCode(request, response);
     }
 
     @Override
