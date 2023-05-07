45bed90fd4fb03f9efe6420e4f50ae3a4b5ba01d
Soslan Gassiev <soslangassity04@gmail.com>
2023-05-07 17:42:54 +0300

    feat : add ExchangeRate query

--- src/main/java/com/gassion/currencyexchange/utils/DBUtils.java
+++ src/main/java/com/gassion/currencyexchange/utils/DBUtils.java
@@ -9,6 +9,11 @@ import java.util.Objects;
 public class DBUtils {
     public static Connection connection = SQLiteConnection.getConnection();
     public static final String SELECT = "SELECT * FROM %s WHERE %s = ?;";
+    public static final String SELECT_EXCHANGE_RATE = "Select * FROM ExchangeRates" +
+            " join Currencies D on D.ID = ExchangeRates.BaseCurrencyId" +
+            " join Currencies C on C.ID = ExchangeRates.TargetCurrencyId" +
+            "WHERE  D.Code = substr(?, 1, 3) and" +
+            "       C.Code = substr(?, 4, 3);";
     public static final String SELECT_ALL = "SELECT * FROM %s;";
     public static final String INSERT = "INSERT INTO %s (%s, %s, %s) VALUES (?, ?, ?);";
     public static final String UPDATE = "UPDATE %s SET %s = ?, %s = ?, %s = ? WHERE ID = ?;";
