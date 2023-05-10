728da1c9ee3a1a4857c1b42f0aa172f8b64c1ce1
Soslan Gassiev <soslangassity04@gmail.com>
2023-05-11 00:43:45 +0300

    refactor : refactor CurrenciesServlet

--- src/main/java/com/gassion/currencyexchange/controllers/currency/CurrenciesServlet.java
+++ src/main/java/com/gassion/currencyexchange/controllers/currency/CurrenciesServlet.java
@@ -1,20 +1,43 @@
 package com.gassion.currencyexchange.controllers.currency;
 
+import com.gassion.currencyexchange.entities.Currency;
+import com.gassion.currencyexchange.entities.factories.CurrencyFactory;
 import com.gassion.currencyexchange.service.CurrencyService;
+import com.gassion.currencyexchange.utils.OutResponse;
+import com.gassion.currencyexchange.utils.ValidateUtils;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet(name = "CurrenciesServlet", value = "/currencies")
 public class CurrenciesServlet extends HttpServlet {
+    private static final ValidateUtils VALIDATE_UTILS = new ValidateUtils();
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        CurrencyService.getAllCurrencies(request, response);
+        try {
+            String currenciesJson = CurrencyService.getAllCurrenciesRequest(request, response);
+
+            OutResponse.setResponse(response, HttpServletResponse.SC_OK, currenciesJson);
+        } catch (SQLException e){
+            OutResponse.setResponse(response, HttpServletResponse.SC_CONFLICT, "Error");
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        CurrencyService.addCurrency(request, response);
+        try {
+            VALIDATE_UTILS.addCurrencyRequestValidate(request);
+            Currency currency  = CurrencyFactory.getInUrl(request.getParameterMap());
+
+            String currencyJson = CurrencyService.addCurrency(currency);
+
+            OutResponse.setResponse(response, HttpServletResponse.SC_OK, currencyJson);
+        } catch (SQLException e) {
+            OutResponse.setResponse(response, e.getErrorCode(), e.getMessage());
+        } catch (Exception s) {
+            OutResponse.setResponse(response, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Error");
+        }
     }
 }
--- src/main/java/com/gassion/currencyexchange/service/CurrencyService.java
+++ src/main/java/com/gassion/currencyexchange/service/CurrencyService.java
@@ -2,7 +2,6 @@ package com.gassion.currencyexchange.service;
 
 import com.gassion.currencyexchange.DAO.CurrencyDAO;
 import com.gassion.currencyexchange.entities.Currency;
-import com.gassion.currencyexchange.entities.factories.CurrencyFactory;
 import com.gassion.currencyexchange.entities.jsonResponse.CurrencyJson;
 import com.gassion.currencyexchange.utils.OutResponse;
 import com.gassion.currencyexchange.utils.ValidateUtils;
@@ -18,35 +17,21 @@ public class CurrencyService {
     private static final Gson GSON = new Gson();
     private static final ValidateUtils VALIDATE_UTILS = new ValidateUtils();
     private static final CurrencyDAO CURRENCY_DAO = new CurrencyDAO();
-    public static void getAllCurrencies(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        try {
+    public static String getAllCurrenciesRequest(HttpServletRequest request, HttpServletResponse response) throws SQLException {
             List<Currency> currencies = CURRENCY_DAO.getAll();
             List<CurrencyJson> currenciesJson = VALIDATE_UTILS.getJsonFormat(currencies);
-            String currenciesList = GSON.toJson(currenciesJson);
-            OutResponse.setResponse(response, HttpServletResponse.SC_OK, currenciesList);
-        } catch (SQLException e){
-            OutResponse.setResponse(response, HttpServletResponse.SC_CONFLICT, "Error");
-        }
+
+            return GSON.toJson(currenciesJson);
     }
 
-    public static void addCurrency(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public static String addCurrency(Currency currency) throws SQLException {
         try {
-            VALIDATE_UTILS.addCurrencyRequestValidate(request);
-
-            Currency currency  = CurrencyFactory.getInUrl(request.getParameterMap());
             CURRENCY_DAO.add(currency);
             currency = CURRENCY_DAO.get(currency.getCode());
 
-            String currencyJson = GSON.toJson(currency.getJsonPesent());
-            OutResponse.setResponse(response, HttpServletResponse.SC_OK, currencyJson);
+            return GSON.toJson(currency.getJsonPesent());
         } catch (SQLException e) {
-            if (e.getErrorCode() == HttpServletResponse.SC_BAD_REQUEST || e.getErrorCode() == HttpServletResponse.SC_NOT_FOUND) {
-                OutResponse.setResponse(response, e.getErrorCode(), e.getMessage());
-            } else {
-                OutResponse.setResponse(response, HttpServletResponse.SC_CONFLICT, "Currency with this code already exists");
-            }
-        } catch (Exception s) {
-            OutResponse.setResponse(response, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Error");
+            throw new SQLException("Currency with this code already exists", "Error", HttpServletResponse.SC_CONFLICT);
         }
     }
 
